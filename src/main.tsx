693f7257e28a72bbe087ff278f47b096b04ccbef
charles <tchazq1n@gmail.com>
2026-04-25 10:58:14 +1000

    Add App.css and update index.css

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { HashRouter } from 'react-router-dom'
 import App from './App'
 import { AuthProvider } from './contexts/AuthContext'
 import './index.css'
+import './App.css'
 
 // Velocity App
 ReactDOM.createRoot(document.getElementById('root')!).render(
